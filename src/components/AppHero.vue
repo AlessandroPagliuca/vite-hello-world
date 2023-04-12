e7c86d3816bc3a0fb54ea3697c103251c607041b
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-12 15:47:31 +0200

    Upgraded AppHero

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,12 +1,16 @@
 <template>
-    <div id="hero">
-
+    <div id="hero" class="c92flex">
+        <AppTitle />
     </div>
 </template>
 
 <script>
+import  AppTitle  from './AppTitle.vue';
     export default {
         name:'AppHero',
+        components:{
+            AppTitle,
+        }
         
     }
 </script>
