ce205fec16acc24fe468f33b8e27cb7a3f146efd
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-04-12 15:40:51 +0200

    Upgraded AppHero

---
+++ src/components/AppHero.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="hero">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'AppHero',
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
